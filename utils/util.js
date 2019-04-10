fbdd2394e0c0ed59e21716639c59486c34c5b9e4
pc <lrpptc@163.com>
2019-04-10 12:10:10 +0800

    删除电影模块

--- utils/util.js
+++ utils/util.js
@@ -12,26 +12,7 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
-function getDate() {
-  var time = new Date()
-  var year = time.getFullYear()
-  var month = time.getMonth()
-  month = month < 10 ? '0' + month : month
-  var day = time.getDay()
-  day = day < 10 ? '0' + day : day
-  return [year, month, day].join('-')
-}
 
-function getTime() {
-  var time = new Date()
-  var hours = time.getHours()
-  hours = hours < 10 ? '0' + hours : hours
-  var minute = time.getMinutes()
-  minute = minute < 10 ? '0' + minute : minute
-  var second = time.getSeconds()
-  second = second < 10 ? '0' + second : second
-  return [hours, minute, second].join(':')
-}
 
 module.exports = {
   formatTime: formatTime,
